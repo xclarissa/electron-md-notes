ca6f4a77b2e7a6fd3add1f208dd582f571f716ab
Clarissa Xavier <clarissaxr@gmail.com>
2024-08-29 15:55:47 -0300

    fixing homedir

--- src/main/lib/index.ts
+++ src/main/lib/index.ts
@@ -9,7 +9,8 @@ import path from 'path'
 import welcomeNote from '../../../resources/welcomeNote.md?asset'
 
 export const getRootDir = () => {
-  return `${homedir()}\\${appDirName}`
+  // return `${homedir()}\\${appDirName}`
+  return path.join(homedir(), appDirName)
 }
 
 export const getNotes: GetNotes = async () => {
